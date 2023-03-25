f65bab89b0e7ee5993e6250bb07bf0919aec76ea
Varun Verma <vermavarun@outlook.com>
2023-03-26 01:03:56 +0530

    ui calling api done

--- Source/ui/src/App.js
+++ Source/ui/src/App.js
@@ -1,32 +1,18 @@
-import logo from './logo.svg';
 import "./App.css";
+
+callApi();
 
 function callApi() {
   fetch("https://vv-api.azurewebsites.net/", { method: "GET" })
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .then((data) => document.getElementsByClassName("App")[0].innerHTML=data);  // Parsing the data into a JavaScript object
+    .then(response => response.text())
+    .then((data) => document.getElementsByClassName("appspan")[0].innerHTML=data);  // Parsing the data into a JavaScript object
 }
 
 function App() {
   return (
-    <div className="App">
-      <button onClick={callApi}>Call API</button>
 
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App">
+     From API: <span className="appspan"></span>
     </div>
   );
 }

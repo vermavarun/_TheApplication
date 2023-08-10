bf848fa34bb7f37388e766c25e1ecb5d6396f53d
Varun Verma <vermavarun@outlook.com>
2023-08-10 13:42:03 +0530

    updated env

--- Source/API/Program.cs
+++ Source/API/Program.cs
@@ -2,7 +2,20 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+var configValue = builder.Configuration.GetValue<string>("urls:ui");
+
+
 builder.Services.AddControllers();
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        builder =>
+        {
+            builder.WithOrigins("https://localhost:3000", configValue)
+                                .AllowAnyHeader()
+                                .AllowAnyMethod();
+        });
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -21,5 +34,5 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
-
+app.UseCors();
 app.Run();

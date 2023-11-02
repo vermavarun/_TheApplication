538d1c84fb204e642ff89c47b00d766ed6c33c8d
Varun Verma <vermavarun@outlook.com>
2023-11-02 13:43:57 +0530

    toast implemented

--- Source/ui/src/App.js
+++ Source/ui/src/App.js
@@ -2,7 +2,6 @@ import "./App.css";
 import Calculator from "./components/calculator/calculator";
 import TopBar from "./components/topbar/topbar";
 import Footer from "./components/footer/footer";
-
 function App() {
   return (
     <>

f6db6180964b3d3af2b9fd5548f94b3a9c904011
Varun Verma <vermavarun@outlook.com>
2023-11-03 12:11:19 +0530

    add bug fixed

--- Source/ui/src/components/calculator/add.js
+++ Source/ui/src/components/calculator/add.js
@@ -5,9 +5,9 @@ function Add(props) {
     var apiUrl =
       process.env.REACT_APP_API_URL + "/add?num1=" + num1 + "&num2=" + num2;
     console.log(apiUrl);
-    
 
-    fetch(apiUrl, { method: "GET", headers: 'Bearer {token}' })
+
+    fetch(apiUrl, { method: "GET" })
       .then((response) => response.text())
       .then((data) => props.onResultCalculated(data))
       .catch( error => console.error(error));

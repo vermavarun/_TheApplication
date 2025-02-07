9ec635818017cf5b31d62c5934895b96c382cb5b
Varun Verma <vermavarun@outlook.com>
2025-02-07 17:10:41 +0530

    Refactor TopNav component to manage login state and update navigation links accordingly

--- Source/next-app/app/components/topnav.tsx
+++ Source/next-app/app/components/topnav.tsx
@@ -4,32 +4,34 @@ import "../components/components.css";
 import { useState, useEffect } from "react";
 
 function TopNav() {
+  const [loggedIn, setLogin] = useState(false);
+
   function logout() {
     localStorage.setItem("loggedIn", "false");
+    setLogin(false);
   }
-  let loggedIn = "false";
   useEffect(() => {
     const loggedIn = localStorage.getItem("loggedIn");
-    console.log("loggedIn", loggedIn);
-    console.log(loggedIn == "true");
+    if (loggedIn === "true") {
+      setLogin(true);
+    }
   }, []);
   return (
     <div className="topnav">
       <a href="/">Home</a>
-      <a href="/admin">Admin</a>
-      {loggedIn === "true" && (
+      {loggedIn}
+      {loggedIn && (
         <>
-          <a hidden={loggedIn === "true"} href="/register">
-            Register
-          </a>
+          <a href="/admin">Admin</a>
 
-          <a href="/login">Login</a>
+          <a onClick={logout}>Logout</a>
         </>
       )}
-      {loggedIn !== "true" && (
-        <a onClick={logout}>
-          Logout
-        </a>
+      {!loggedIn && (
+        <>
+          <a href="/register">Register</a>
+          <a href="/login">Login</a>
+        </>
       )}
     </div>
   );

cd1e9e094b39efa4fd63c25d70dc5a8012739c9a
Varun Verma <vermavarun@outlook.com>
2025-02-09 16:43:27 +0530

    Remove commented-out methods from AdminController and add unit tests for user retrieval

--- Source/identity/Controllers/AdminController.cs
+++ Source/identity/Controllers/AdminController.cs
@@ -25,54 +25,5 @@ namespace Admin.Controllers
             return Ok(users);
         }
 
-        // [HttpPost]
-        // public async Task<IActionResult> PostUser([FromBody] IdentityUser user)
-        // {
-        //     var result = await _userManager.CreateAsync(user);
-        //     return Ok(result);
-        // }
-
-        // [HttpDelete]
-        // public async Task<IActionResult> DeleteUser([FromBody] IdentityUser user)
-        // {
-        //     var result = await _userManager.DeleteAsync(user);
-        //     return Ok(result);
-        // }
-
-        // [HttpPut]
-        // public async Task<IActionResult> PutUser([FromBody] IdentityUser user)
-        // {
-        //     var result = await _userManager.UpdateAsync(user);
-        //     return Ok(result);
-        // }
-
-        // [HttpGet]
-        // public async Task<IActionResult> GetRoles()
-        // {
-        //     var roles = await _roleManager.Roles.ToListAsync();
-        //     return Ok(roles);
-        // }
-
-        // [HttpPost]
-        // public async Task<IActionResult> PostRole([FromBody] IdentityRole role)
-        // {
-        //     var result = await _roleManager.CreateAsync(role);
-        //     return Ok(result);
-        // }
-
-        // [HttpDelete]
-        // public async Task<IActionResult> DeleteRole([FromBody] IdentityRole role)
-        // {
-        //     var result = await _roleManager.DeleteAsync(role);
-        //     return Ok(result);
-        // }
-
-        // [HttpPut]
-        // public async Task<IActionResult> PutRole([FromBody] IdentityRole role)
-        // {
-        //     var result = await _roleManager.UpdateAsync(role);
-        //     return Ok(result);
-        // }
-
     }
 }

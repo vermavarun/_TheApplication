b02c69140b3939e1af357ec3f1c9cd57f0bbc145
Varun Verma <vermavarun@outlook.com>
2025-02-16 14:03:57 +0530

    Add TopNavUser component for user avatar and logout functionality

--- Source/next-app/app/components/topnav.tsx
+++ Source/next-app/app/components/topnav.tsx
@@ -8,22 +8,11 @@ import Link from "next/link";
 import { useRouter } from "next/navigation";
 import toast from "react-hot-toast";
 import React from "react";
+import TopNavUser from "./topNavUser";
 
 function TopNav() {
   const userValue:User = useAppSelector((state) => state.user);
-  const dispatch = useAppDispatch();
-  const router = useRouter()
 
-  function logout() {
-    localStorage.removeItem("userType");
-    localStorage.removeItem("github_token");
-    localStorage.removeItem("google_token");
-    localStorage.removeItem("accessToken");
-    localStorage.removeItem("latestCSRFToken");
-    dispatch(setUserSlice({}));
-    toast.success("Logout successful");
-    router.push("/login");
-  }
 
   return (
     <div className="topnav">
@@ -33,26 +22,17 @@ function TopNav() {
         </div>
 
         <div className="topnav-pages">
-        <Link href="/">Home</Link>
-        <Link href="/admin">Admin</Link>
-          {
-            !userValue?.name &&
-            <React.Fragment key={"topnav-register"}>
-              <Link href="/register">Register</Link>
-              <Link href="/login">Login</Link>
-            </React.Fragment>
-          }
+          <Link href="/">Home</Link>
+          <Link href="/admin">Admin</Link>
+            {
+              !userValue?.name &&
+              <React.Fragment key={"topnav-register"}>
+                <Link href="/register">Register</Link>
+                <Link href="/login">Login</Link>
+              </React.Fragment>
+            }
         </div>
-
-
-        <div className="topnav-user">
-            <div className="topnav-user-avatar">
-              <img src={userValue?.avatar_url ? userValue?.avatar_url : '/static/images/avatar_logoff.avif'} alt="avatar" referrerPolicy="no-referrer" />
-            </div>
-
-            {userValue?.name && <><div className="topnav-user-name">{userValue?.name}</div> <div className="topnav-user-logout" style={{cursor:"pointer"}} onClick={logout}>Logout</div></>}
-        </div>
-
+        <TopNavUser />
     </div>
   );
 }
